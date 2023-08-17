f2bf88dff63dd4ded833a2a87342930800364791
haybek <haybekbatuhan@gmail.com>
2023-08-17 13:00:39 +0300

    Add project files.

---
+++ CokBasitUyelikSistemi/Controllers/GirisController.cs
@@ -0,0 +1,32 @@
+﻿using CokBasitUyelikSistemi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CokBasitUyelikSistemi.Controllers
+{
+    public class GirisController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost,ValidateAntiForgeryToken]
+        public IActionResult Index(Kullanici kullanici)
+        {
+
+            if (ModelState.IsValid)
+            {
+                if (kullanici.KullaniciAdi == "ali" && kullanici.Parola == "123")
+                {
+                    // bu istegi yaparn tarayici tarafindan giris yapildigini not al
+
+                    return RedirectToAction("Index", "Home", new { giris = "basarili"});
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Kullanici adi yada parola yanlis");
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ CokBasitUyelikSistemi/Models/Kullanici.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CokBasitUyelikSistemi.Models
+{
+    public class Kullanici
+    {
+        [Required]
+        public string KullaniciAdi { get; set; } = null!;
+        [Required]
+        [DataType(DataType.Password)]
+        public string Parola { get; set; } = null!;
+    }
+}
